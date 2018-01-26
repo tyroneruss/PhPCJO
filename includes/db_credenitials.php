1e4c2eced0731b75490699279ee2123f0a50e82f
Tyrone Russ <tyroneruss@outlook.com>
2018-01-25 20:21:45 -0500

    change mysql user to root

--- includes/db_credenitials.php
+++ includes/db_credenitials.php
@@ -5,8 +5,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-define("DB_SERVER","localhost");
-define("DB_USER","cjoroot");
+define("DB_SERVER","localhost:3306");
+define("DB_USER","root");
 define("DB_PASS","rhouse11");
 define("DB_NAME","cjodb");
 
